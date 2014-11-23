c477283fc08da4080f40f2b8d57b81f99e269398
Vitaly Omelchenko <vomelchenko@productengine.com>
2014-11-23 13:43:56 +0200

    update UI

--- app/helpers/my_forum/application_helper.rb
+++ app/helpers/my_forum/application_helper.rb
@@ -1,4 +1,25 @@
 module MyForum
   module ApplicationHelper
+
+    def draw_navigation
+      html_li  = content_tag :li, link_to('Forums', root_path)
+
+      if @forum
+        html_li += content_tag :li, '&rarr;'.html_safe
+        html_li += content_tag :li, link_to(@forum.name, forum_path(@forum))
+      end
+
+      if @forum && @topic && !@topic.new_record?
+        html_li += content_tag :li, '&rarr;'.html_safe
+        html_li += content_tag :li, link_to(@topic.name, forum_topic_path(@forum, @topic))
+      end
+
+      html = content_tag :ul, class: 'draw-navigation' do
+        html_li
+      end
+
+      html.html_safe
+    end
+
   end
 end
