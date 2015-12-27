65be31941f3af579208d7907eceb8767c4e4836b
Vitaly Omelchenko <prosto.vint@gmail.com>
2015-12-27 22:47:30 +0200

    fix last messages access forums
    Signed-off-by: Vitaly Omelchenko <prosto.vint@gmail.com>

--- app/controllers/my_forum/welcome_controller.rb
+++ app/controllers/my_forum/welcome_controller.rb
@@ -20,7 +20,7 @@ module MyForum
                           SELECT posts.id, posts.user_id, posts.text, posts.topic_id, posts.updated_at, topics.name as topic_name, topics.forum_id, topics.deleted FROM my_forum_posts AS posts
                           LEFT JOIN my_forum_topics AS topics ON posts.topic_id = topics.id
                           LEFT JOIN my_forum_forums AS forums ON forums.id = topics.forum_id
-                          WHERE posts.id IN (SELECT MAX(id) FROM my_forum_posts GROUP BY topic_id) AND posts.is_deleted IS FALSE AND topics.deleted IS FALSE
+                          WHERE posts.id IN (SELECT MAX(id) FROM my_forum_posts GROUP BY topic_id) AND posts.is_deleted IS FALSE AND topics.deleted IS FALSE AND forums.id IN (#{available_forum_ids.join(',')})
                           ORDER BY posts.id DESC LIMIT 10
                         ")
                       end
